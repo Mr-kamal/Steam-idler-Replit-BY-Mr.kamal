4d6ddb30680527668b4ef36d1a0ae596394b342c
Kamal <37162268+Mr-kamal@users.noreply.github.com>
2024-01-16 13:02:32 +0100

    Update index.js
    Adding Games:
    -7 Days to Die
    -Titanfall® 2
    -Portal 2
    -A Story About My Uncle

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [582660, 440, 570,278360,];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [582660, 440, 570, 278360, 620, 1237970, 251570,];  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
